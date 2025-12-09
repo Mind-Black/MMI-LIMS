5cfa43a0da8834dff32e7be4d176b9e9b15ba87f
Mind-Black <113443563+Mind-Black@users.noreply.github.com>
2025-12-09 11:54:09 +0200

    Change START_HOUR to 0 for calendar events

--- src/components/UserBookingsCalendar.jsx
+++ src/components/UserBookingsCalendar.jsx
@@ -51,7 +51,7 @@ const UserBookingsCalendar = ({ bookings, currentWeekStart, onWeekChange, onBook
     }, [bookings]);
 
     const PIXELS_PER_30_MINS = 48;
-    const START_HOUR = 8;
+    const START_HOUR = 0;
 
     const getEventStyle = (booking) => {
         const startHour = parseInt(booking.startTime.split(':')[0]);
